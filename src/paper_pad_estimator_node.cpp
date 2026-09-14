5ee2a41606398ea19d05fd95318a6810a4d3cadf
sanghun17 <sanghun17@users.noreply.github.com>
2026-09-14 17:17:52 +0900

    Support standard ArUco dictionary families

--- src/paper_pad_estimator_node.cpp
+++ src/paper_pad_estimator_node.cpp
@@ -91,6 +91,18 @@ int dictionaryId(const std::string& name) {
       {"DICT_4X4_100", cv::aruco::DICT_4X4_100},
       {"DICT_4X4_250", cv::aruco::DICT_4X4_250},
       {"DICT_4X4_1000", cv::aruco::DICT_4X4_1000},
+      {"DICT_5X5_50", cv::aruco::DICT_5X5_50},
+      {"DICT_5X5_100", cv::aruco::DICT_5X5_100},
+      {"DICT_5X5_250", cv::aruco::DICT_5X5_250},
+      {"DICT_5X5_1000", cv::aruco::DICT_5X5_1000},
+      {"DICT_6X6_50", cv::aruco::DICT_6X6_50},
+      {"DICT_6X6_100", cv::aruco::DICT_6X6_100},
+      {"DICT_6X6_250", cv::aruco::DICT_6X6_250},
+      {"DICT_6X6_1000", cv::aruco::DICT_6X6_1000},
+      {"DICT_7X7_50", cv::aruco::DICT_7X7_50},
+      {"DICT_7X7_100", cv::aruco::DICT_7X7_100},
+      {"DICT_7X7_250", cv::aruco::DICT_7X7_250},
+      {"DICT_7X7_1000", cv::aruco::DICT_7X7_1000},
   };
   const auto found = dictionaries.find(name);
   if (found == dictionaries.end()) {
